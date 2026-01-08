bc7d5da0885c68a4a0827eebfc7974f55f17859d
ctcest43basket <ctcest43basket@gmail.com>
2026-01-08 15:45:55 +0100

    Create index.js

---
+++ ctc-est43-main/pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>CTC EST43 Basket</h1>
+      <p>Site officiel</p>
+      <p>Entente Monistrol-sur-Loire & Sainte-Sigolène</p>
+    </div>
+  );
+}
+
