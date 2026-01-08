1da879c88989abfb4900daed2b786585e39869db
ctcest43basket <ctcest43basket@gmail.com>
2026-01-08 11:56:31 +0100

    Add files via upload

---
+++ ctc-est43-main/README.md
@@ -0,0 +1,4 @@
+# CTC EST43 Basket
+
+Site officiel de la CTC EST43 Basket.
+Projet prêt à être déployé sur Vercel.
---
+++ ctc-est43-main/app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>CTC EST43 Basket</h1>
+      <p>Site officiel de la CTC EST43 (Monistrol-sur-Loire & Sainte-Sigolène)</p>
+    </main>
+  );
+}
